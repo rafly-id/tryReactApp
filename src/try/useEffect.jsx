a6330459b1fe33a314e9beb326c15033c2ae6c33
rafly <raflym@gmail.com>
2025-03-21 17:59:33 +0700

    make another useEffect

--- src/try/useEffect.jsx
+++ src/try/useEffect.jsx
@@ -19,6 +19,29 @@ function MyComponent() {
   function changeColor() {
     setColor((c) => (c === "green" ? "red" : "green"));
   }
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    console.log("Event Listener Add");
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      console.log("Event Listener Remove");
+    };
+  }, []);
+
+  useEffect(() => {
+    document.title = `Size: ${width} + ${height}`;
+  }, [width, height]);
+
+  function handleResize() {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  }
+
   return (
     <>
       <p style={{ color: color }}>Count: {count}</p>
@@ -26,6 +49,9 @@ function MyComponent() {
       <button onClick={subtractCount}>Subtract</button>
       <br />
       <button onClick={changeColor}>Color Change</button>
+      <br />
+      <p>Width : {width}</p>
+      <p>Height : {height}</p>
     </>
   );
 }
